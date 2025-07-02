3d6fc01ead20baa76f8a99dfd92b944c58c7a9dc
Gelong-GH <gelohaban0819@gmail.com>
2025-07-02 15:38:27 +0800

    Add files via upload

---
+++ Activity1.js
@@ -0,0 +1,16 @@
+//variables activity
+
+/*1. Create a variable named name and store your name.
+
+2. Create a variable named whatDoYouWannaBecomeInYourLife and store
+"programmer".
+3. Create a variable named gender and store your gender.
+4. Create a variable named twitterHandle and store your Twitter handle.
+5. Log all the variables to the console using the code runner in VSCode.*/
+
+name = "Mark Angelo"; // i hav variable "name" and i am storin my name.
+whatDoYouwannaBecomeInYourLife = "Game Developer"; // same here, variable "whatDoYouwannaBecomeInYourLife" and i stored programmer.
+gender = "male"; // variable gender and i am a male.
+facebookHandle = "Mark Angelo T. Haban"; // same-same here var facebookhandle and i stored my facebook account name.
+console.log("My Name is", name, "and I want to become a" , whatDoYouwannaBecomeInYourLife, " and my facebook is", facebookHandle);
+ // last line here is to print my stored variables.i just added some sstring to make a sentence
